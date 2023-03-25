b3ef26805e83cce70d58bdd182337ec0596eef12
MaxMax222 <126956833+MaxMax222@users.noreply.github.com>
2023-03-25 17:14:40 +0300

    question 22 done

---
+++ Q22/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers;
+            int len;
+            Console.Write("enter the length of the array: ->");
+            len = int.Parse(Console.ReadLine());
+            numbers = new int[len];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"enter the {i + 1} number: ->");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(numbers);
+            Console.WriteLine($"the sorted array: {string.Join(",", numbers)}");
+        }
+    }
+}
