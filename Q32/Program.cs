4c07d8bff7cf5848cf3e1a5050a24b957e543994
MaxMax222 <126956833+MaxMax222@users.noreply.github.com>
2023-03-25 17:04:07 +0300

    first commit, question 32 done

---
+++ Q32/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q32
+{
+    class Program
+    {
+        static bool ZigZag(int[] arr)
+        {
+            bool checkOption1 = false, checkOption2 = false;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    checkOption1 = arr[i] < arr[i + 1]; 
+                }
+                else
+                {
+                    checkOption1 = arr[i] > arr[i + 1];
+                }
+            }
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    checkOption2 = arr[i] > arr[i + 1];
+                }
+                else
+                {
+                    checkOption2 = arr[i] < arr[i + 1];
+                }
+            }
+            return checkOption1 || checkOption2;
+        }
+        static void Main(string[] args)
+        {
+
+            int[] arr1 = { 8, 70, 5, 20, 6, 40, 3 };
+            int[] arr2 = { 80, 6, 40, 30, 50, 20 };
+            if (ZigZag(arr1))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            if (ZigZag(arr2))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
